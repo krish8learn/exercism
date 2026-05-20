b91f792ad91a0bb3d49bacbfd0f6d94139819b0d
krishnendu karmakar <krish9857@outlook.com>
2026-05-20 20:47:29 +0530

    more rust challnges

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,11 @@
+pub struct Clock;
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        todo!("Add {minutes} minutes to existing Clock time");
+    }
+}
